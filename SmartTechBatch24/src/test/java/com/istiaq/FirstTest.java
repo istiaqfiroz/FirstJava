d6c0584c5ddf2a4ddbb1215c6aee6a8de54afeaf
i=Istiaq Firoz <istiaqfiroz.du@gmail.com>
2022-09-16 22:50:04 -0400

    develop edit

--- SmartTechBatch24/src/test/java/com/istiaq/FirstTest.java
+++ SmartTechBatch24/src/test/java/com/istiaq/FirstTest.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Hello Java");
 	System.out.println("Test me");
 	System.out.println("Test2");
+	System.out.println("Develop Branh");
 }
 }
